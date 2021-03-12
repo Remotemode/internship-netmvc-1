25eb1a5cbe36e5f18731ec553453a2158ccfa6db
Garazha <garaja9911@gmail.com>
2021-03-12 13:06:40 +0300

    Create Project.
    Create Project.

---
+++ Sharetrade/Sharetrade/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sharetrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
