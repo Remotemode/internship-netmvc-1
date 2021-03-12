0f24c0640ad5c7f153b83a45628d2f07d6e54797
Garazha <garaja9911@gmail.com>
2021-03-12 18:27:01 +0300

    Pages & basic layout.
    Pages & basic layout.

---
+++ Sharetrade/Sharetrade/Models/Article.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sharetrade.Models
+{
+    public class Article
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("imageUrl")]
+        public string ImageUrl { get; set; }
+
+        [JsonPropertyName("expertComment")]
+        public string ExpertComment { get; set; }
+
+        public Article() { }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj.GetType() != this.GetType()) return false;
+
+            var that = obj as Article;
+
+            if (Title != that.Title) return false;
+
+            if (Description != that.Description) return false;
+
+            if (ImageUrl != that.ImageUrl) return false;
+
+            return ExpertComment == that.ExpertComment;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Title != null ? Title.GetHashCode() : 0;
+                hashCode = (hashCode * 375) ^ (Description != null ? Description.GetHashCode() : 0);
+                hashCode = (hashCode * 375) ^ (ImageUrl != null ? ImageUrl.GetHashCode() : 0);
+                hashCode = (hashCode * 375) ^ (ExpertComment != null ? ExpertComment.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+}
--- Sharetrade/Sharetrade/Program.cs
+++ Sharetrade/Sharetrade/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Sharetrade
 {
@@ -13,7 +9,16 @@ namespace Sharetrade
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
